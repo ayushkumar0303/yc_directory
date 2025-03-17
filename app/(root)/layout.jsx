7f7401b64715d8f7e809d185a7fa766a681b929a
Ayush Kumar <ayushk8295@gmail.com>
2025-03-18 02:05:27 +0530

    make basic layouts and setup

---
+++ app/(root)/layout.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Layout({ children }) {
+  return <main className="font-work-sans">{children}</main>;
+}
+
+export default Layout;
---
+++ app/(root)/page.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  console.log();
+  return (
+    <>
+      <h1 className="">Welcome to next js</h1>
+    </>
+  );
+}
